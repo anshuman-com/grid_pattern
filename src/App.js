b90ae16d0ed46f5a14d13f2e6e2869142a4652d5
anshuman <anshuman.gomicro@gmail.com>
2024-11-13 20:29:59 +0530

    fixed eroor in deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import ScrollingTextGrid from './components/ScrollingTextGrid';
-import MovingTextGrid from "./components/MovingTextGrid";
 function App() {
     return (
         <div className="App">
             <ScrollingTextGrid />
-            <MovingTextGrid/>
         </div>
     );
 }
